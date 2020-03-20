cf6de85f89766ddcb1495d6352ddff2fc6ae21f0
warren <warrenb95@live.co.uk>
2020-03-20 12:58:08 +0000

    Init commit

---
+++ DOTNET_TODO_API/DOTNET_TODO_API/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DOTNET_TODO_API.Models
+{
+    public class Todo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public DateTime Created { get; set; }
+        public bool Completed { get; set; }
+    }
+}
